e5ab1a5ac3fa3f6a782170ef3b9c6047c2cc3915
Gabrielbarbosa14 <terezamariabarbosa635@gmail.com>
2026-05-19 11:07:08 -0300

    aula 08

--- js/main.js
+++ js/main.js
@@ -4,13 +4,23 @@ import {
   limparInput,
   renderizarTarefas,
   exibirMensagem,
+  exibirDica,
 } from "./dom.js";
 
 //importando funções do módulo tarefas
 import { validarTarefa, adicionarTarefa, obterTarefas } from "./tarefas.js";
 
+// Importando função para buscar dica
+import { buscarDica } from "./api.js";
+
 //selecionar o formulario para adicionar um evento de submit
 const form = document.querySelector("#form-tarefa");
+
+// Função para iniciar a aplicação, buscando uma dica e exibindo-a
+async function iniciarAplicacao() {
+  const dica = await buscarDica();
+  exibirDica(dica);
+}
 
 //evento de submit para adicionar uma nova tarefa
 form.addEventListener("submit", function (event) {
@@ -29,3 +39,6 @@ form.addEventListener("submit", function (event) {
   exibirMensagem("tarefa adicionada com sucesso!", "sucesso");
   limparInput();
 });
+
+// Iniciar a aplicação ao carregar a página
+iniciarAplicacao();
